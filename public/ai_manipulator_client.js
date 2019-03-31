7540e23839f44964e4160fcae76ebd9d294d0d71
EricS <esimon55960@gmail.com>
2019-03-31 17:58:45 -0500

    Validate screen name before allowing rename

--- public/ai_manipulator_client.js
+++ public/ai_manipulator_client.js
@@ -12,10 +12,22 @@ function closeMessage() {
 
 function showWaiting(msg) {
     _showOverlay('waiting.gif', msg)
+    return true;
 }
 
 function _showOverlay(icon, msg) {
     document.getElementById('overlayIcon').src = '/public/' + icon;
     document.getElementById('overlayMessage').innerText = msg;
     document.getElementById('overlay').style.display = 'block';
+}
+
+function validScreenName(form) {
+    const newName = form.newName.value;
+    const isValidPattern = new RegExp('^[a-zA-Z0-9_]+$');
+    if (!isValidPattern.test(newName)) {
+        displayAnyMessage({icon: 'icons8-error-30.png', message: 'Screen name must be a-z, A-Z, 0-9 or _ characters.'})
+        return false;
+    } else {
+        return true;
+    }
 }
